d18ac336af835a6dbea3b1a3d346148f11b46e2d
Nitin <52646268+Nitin1818@users.noreply.github.com>
2020-09-08 08:35:56 +0530

    [Fun] New Module
    Signed-off-by: Nitin1818 <xbinner18@outlook.com>

---
+++ userbot/modules/fun.py
@@ -0,0 +1,30 @@
+from telethon.tl import types
+from userbot.events import register
+from userbot import bot
+
+@register(outgoing=True, pattern="^.dart$")
+async def dart(event):
+  await event.edit("`Sending dart...`")
+  await event.delete()
+  await bot.send_file(event.chat.id, types.InputMediaDice('🎯'))
+
+
+@register(outgoing=True, pattern="^.dice$")
+async def dart(event):
+  await event.edit("`Sending dice...`")
+  await event.delete()
+  await bot.send_file(event.chat.id, types.InputMediaDice('🎲'))
+  
+  
+@register(outgoing=True, pattern="^.ball$")
+async def dart(event):
+  await event.edit("`Sending basketbaawait`")
+  await event.delete()
+  await bot.send_file(event.chat.id, types.InputMediaDice('🏀'))
+  
+  
+@register(outgoing=True, pattern="^.fball$")
+async def dart(event):
+  await event.edit("`Sending football...`")
+  await event.delete()
+  await bot.send_file(event.chat.id, types.InputMediaDice('⚽'))
