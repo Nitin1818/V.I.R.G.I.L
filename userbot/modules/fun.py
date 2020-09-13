0ff0d0e04bbe549ea59faa58e4d20173d14192f6
Nitin <52646268+Nitin1818@users.noreply.github.com>
2020-09-13 09:09:48 +0530

    Cleanup

--- userbot/modules/fun.py
+++ userbot/modules/fun.py
@@ -2,29 +2,30 @@ from telethon.tl import types
 from userbot.events import register
 from userbot import bot
 
+
 @register(outgoing=True, pattern="^.dart$")
 async def dart(event):
-  await event.edit("`Sending dart...`")
+  await event.edit("Sending dart")
   await event.delete()
-  await bot.send_file(event.chat.id, types.InputMediaDice('🎯'))
+  await bot.send_file(event.chat.id, types.InputMediaDice("🎯"))
 
 
 @register(outgoing=True, pattern="^.dice$")
 async def dice(event):
-  await event.edit("`Sending dice...`")
+  await event.edit("Sending dice")
   await event.delete()
-  await bot.send_file(event.chat.id, types.InputMediaDice('🎲'))
+  await bot.send_file(event.chat.id, types.InputMediaDice("🎲"))
   
   
 @register(outgoing=True, pattern="^.ball$")
 async def ball(event):
-  await event.edit("`Sending basketbaawait`")
+  await event.edit("Sending basketball")
   await event.delete()
-  await bot.send_file(event.chat.id, types.InputMediaDice('🏀'))
+  await bot.send_file(event.chat.id, types.InputMediaDice("🏀"))
   
   
 @register(outgoing=True, pattern="^.fball$")
 async def fball(event):
-  await event.edit("`Sending football...`")
+  await event.edit("Sending football")
   await event.delete()
-  await bot.send_file(event.chat.id, types.InputMediaDice('⚽'))
+  await bot.send_file(event.chat.id, types.InputMediaDice("⚽"))
